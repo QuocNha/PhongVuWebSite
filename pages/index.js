2ccc7d678d0846ccadf17bf93b0f989949055264
nguoitacu@gmail.com <nguoitacu@gmail.com>
2021-05-28 13:42:19 +0700

    error key on consle

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="root">
-        <HomePage></HomePage>
+        <HomePage key="HomePage"></HomePage>
       </div>
     </React.Fragment>
   )
