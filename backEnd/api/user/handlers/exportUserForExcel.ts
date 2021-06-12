07070efa29940a1e1ca981a6ef09a9e3e82f58f5
nguoitacu@gmail.com <nguoitacu@gmail.com>
2021-06-13 02:16:51 +0700

    change domain

--- backEnd/api/user/handlers/exportUserForExcel.ts
+++ backEnd/api/user/handlers/exportUserForExcel.ts
@@ -8,6 +8,7 @@ import  excel from 'node-excel-export';
 import dbConnect from '../../../../utils/dbConnect';
 import User from "../../../model/user";
 import moment from 'moment';//BZ00029
+import nodeExcel from 'excel-export-next';//BZ00029
 
 // You can define styles as json object
 const styles = {
@@ -106,49 +107,98 @@ const merges = [
 // Create the excel report.
 // This function will return Buffer
 const exportUserForExcel = async({res,req,body: { user_cookies , check_token }}) => {
-    await dbConnect(); 
-    let data=[];
-    const dataset  = await User.find({}).sort({
-        createAt: "desc",
-      });
-      const user = await User.find({}) 
-      .sort({
-          createAt: "desc",
-        });
-      //   console.log("req.query",req.query.page);
-      //   console.log("req.query",user);
-        for(let i=0 ;i<user.length;i++){
-            data.push(
-              {
-                  _id:user[i]._id,
-                  userName:user[i].email,
-                  number:i+1,
-                  userRole:user[i].userRole,
-                  img:user[i].img,
-                  createAt: moment(user[i].createAt).format('DD-MM-YYYY'),//BZ00029
-                  userType:user[i].userType
-              }
-          )
-       }
+    // await dbConnect(); 
+    // let data=[];
+    // const dataset  = await User.find({}).sort({
+    //     createAt: "desc",
+    //   });
+    //   const user = await User.find({}) 
+    //   .sort({
+    //       createAt: "desc",
+    //     });
+    //   //   console.log("req.query",req.query.page);
+    //   //   console.log("req.query",user);
+    //     for(let i=0 ;i<user.length;i++){
+    //         data.push(
+    //           {
+    //               _id:user[i]._id,
+    //               userName:user[i].email,
+    //               number:i+1,
+    //               userRole:user[i].userRole,
+    //               img:user[i].img,
+    //               createAt: moment(user[i].createAt).format('DD-MM-YYYY'),//BZ00029
+    //               userType:user[i].userType
+    //           }
+    //       )
+    //    }
       
 
-    const report = excel.buildExport(
-        [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
-          {
-            name: 'Report', // <- Specify sheet name (optional)
-            heading: heading, // <- Raw heading array (optional)
-           // merges: merges, // <- Merge cell ranges
-            specification: specification, // <- Report specification
-            data: data // <-- Report data
-          }
-        ]
-      );
-      // You can then return this straightr
+    // const report = excel.buildExport(
+    //     [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
+    //       {
+    //         name: 'Report', // <- Specify sheet name (optional)
+    //         heading: heading, // <- Raw heading array (optional)
+    //        // merges: merges, // <- Merge cell ranges
+    //         specification: specification, // <- Report specification
+    //         data: data // <-- Report data
+    //       }
+    //     ]
+    //   );
+    //   // You can then return this straightr
       
-      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');  
-      res.send(report);  
-      res.end();
-  
+    //   res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');  
+    //   res.send(report);  
+    //   res.end();
+    var conf ={
+      stylesXmlFile:"",
+      name:"",
+      cols:[],
+      rows:[]
+    };
+    // conf.stylesXmlFile = "styles.xml";
+      conf.name = "mysheet";
+      conf.cols = [{
+      caption:'string',
+          type:'string',
+          beforeCellWrite:function(row, cellData){
+         return cellData.toUpperCase();
+      },
+          width:28.7109375
+    },{
+      caption:'date',
+      type:'date',
+      beforeCellWrite:function(){
+        return function(row, cellData, eOpt){
+                if (eOpt.rowNum%2){
+                  eOpt.styleIndex = 1;
+                }  
+                else{
+                  eOpt.styleIndex = 2;
+                }
+                  if (cellData === null){
+                    eOpt.cellType = 'string';
+                    return 'N/A';
+                  } else
+                    return (cellData) / (24 * 60 * 60 * 1000);
+        } 
+      }()
+    },{
+      caption:'bool',
+      type:'bool'
+    },{
+      caption:'number',
+       type:'number'				
+      }];
+      conf.rows = [
+       ['pi', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
+       ["e", new Date(2012, 4, 1), false, 2.7182],
+          ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+          ["null date", null, true, 1.414]  
+      ];
+      var result = nodeExcel.execute(conf);
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+      res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+      res.end(result, 'binary');
 }
 export default exportUserForExcel;
  
