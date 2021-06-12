f2f111952b4c76e88a9fab85d9425e6fca9c8571
nguoitacu@gmail.com <nguoitacu@gmail.com>
2021-06-12 16:00:05 +0700

    Change domain

--- constant.config.api/config/config.ts
+++ constant.config.api/config/config.ts
@@ -8,9 +8,9 @@
 
 ************************************************************************
 */
-const SERVERAPI = "http://localhost:3000/";
+//const SERVERAPI = "http://localhost:3000/";
 // bang3 LIve
-//const SERVERAPI ="https://order-demo-nextjs.herokuapp.com/";
+const SERVERAPI ="https://order-demo-nextjs.herokuapp.com/";
 
 const LOGINUSER = SERVERAPI+ "api/login";//BZ00017
 const CHECK_TOKENAPI = SERVERAPI+ "api/checkTokenAPI";//BZ00019
